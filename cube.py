254b5d7c97d4815b072b5f680e83e58495cdfdf1
Agnieszka Wojdecka <77381@student.ue.poznan.pl>
2023-06-03 09:36:30 +0200

    Test

--- cube.py
+++ cube.py
@@ -3,4 +3,7 @@ def cube_calculations(a):
     objetosc=a**3
     return pole, objetosc
 
-print(cube_calculations(7)) 
+print(cube_calculations(4)) 
+
+assert cube_calculations(7) == (294, 343)
+assert cube_calculations(4) == (96, 64)
