c706e404d8748a265ba8fb251d9a0892918225f7
Wojtek Marszałek <wojtekmarszalek246@gmail.com>
2023-06-03 10:56:47 +0200

    ostroslup

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ print("Objetosc kuli wynosi " + str(objetosc_kuli(5)))
 
 assert objetosc_kuli(5)==523.33
 assert objetosc_kuli(8)==2143.57
+
+#ostroslup
+import math
+def ostroslup_trojkatny(a,h,H):
+    pole=round((math.pow(a,2)*math.sqrt(3))/4+(3*a*h)/2,2)
+    objetosc=round((math.pow(a,2)*math.sqrt(3)*H)/12,2)
+
+    return pole, objetosc
+
+print(ostroslup_trojkatny(3,4,5))
+
+assert ostroslup_trojkatny(5,6,7)==(55.83,25.26)
+assert ostroslup_trojkatny(7,9,10)==(115.72,70.73)
