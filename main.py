d257ee7827acfc9f7f0668977705e62b016efc85
Agnieszka Wojdecka <77381@student.ue.poznan.pl>
2023-06-03 11:24:01 +0200

    Merged stozek

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+#ostroslup
+import math
+
+def objetosc_stozka(r, h):
+    objetosc = 1/3 * 3.14 * r**2 * h
+    return objetosc
+
+print(objetosc_stozka(3, 100))
+
+assert objetosc_stozka(3, 100) == 942
+
 def cube_calculations(a):
     pole=6*(a**2)
     objetosc=a**3
